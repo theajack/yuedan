bc3ff04390c17d33d9a8d0f4d34fbdcb468932cb
theajack <1506282385@qq.com>
2017-07-15 14:28:19 +0800

    17-7-15

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -6,6 +6,9 @@ var _h=J.height();
 var _n=30;
 var ctx = canvas.getContext("2d"), canvas_width, canvas_height;
 var circles=new Array();
+var frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(func) {
+        window.setTimeout(func, 1000 / 45);
+    };
 (function () {
     canvas.width=_w;
     canvas.height=_h;
@@ -20,60 +23,63 @@ var circles=new Array();
     _draw();
 })();
 function _draw() {
-    setInterval(function () {
-        ctx.clearRect(0, 0, _w, _h);
-        circles.each(function (item) {
-            item.act();
-        });
-    },100)//
+    ctx.clearRect(0, 0, _w, _h);
+    circles.each(function (item) {
+        item.act();
+    });
+    frame_func(_draw);
 }
 function _init() {
   this.x=J.random(0,_w);
   this.y=J.random(0,_h);
   this.r=0;
   this.max_r=J.random(10,50);
-  this.speed=J.random(2,6)*0.1;
+  this.speed=J.random(2,6)*0.02;
   this.alpha=J.random(2,9)*0.1;
   this.per_a=0;
   this.color_base=((J.random(0,1)==0)?g_color:b_color);
   this.color=this.color_base+this.alpha+")";
   this.stop=false;
+
 }
 function Circle() {
-  _init.call(this);
-};Circle.prototype.reinit=function(){
-  _init.call(this);
-};Circle.prototype.act=function(){
-    if(!this.stop) {
-        this.r += this.speed;
-        if (this.r > this.max_r * 0.7) {
-            if (this.r >= this.max_r) {
-                this.stop = true;
-                this.reinit();
-            } else {
-                if (this.alpha > 0) {
-                    if (this.per_a == 0) {
-                        this.per_a = (this.alpha) / ((this.max_r - this.r) / this.speed);
-                    }
-                    this.alpha -= this.per_a;
-                    if (this.alpha <= 0) {
-                        this.alpha=0;
-                    }
-                    this.color = this.color_base + this.alpha + ")";
+    _init.call(this);
+    this.reinit=function(){
+        _init.call(this);
+    };
+    this.act=function(){
+        if(!this.stop) {
+            this.r += this.speed;
+            if (this.r > this.max_r * 0.7) {
+                if (this.r >= this.max_r) {
+                    this.stop = true;
+                    this.reinit();
                 } else {
-                    this.alpha=0;
-                    this.color = this.color_base + this.alpha + ")";
+                    if (this.alpha > 0) {
+                        if (this.per_a == 0) {
+                            this.per_a = (this.alpha) / ((this.max_r - this.r) / this.speed);
+                        }
+                        this.alpha -= this.per_a;
+                        if (this.alpha <= 0) {
+                            this.alpha=0;
+                        }
+                        this.color = this.color_base + this.alpha + ")";
+                    } else {
+                        this.alpha=0;
+                        this.color = this.color_base + this.alpha + ")";
+                    }
                 }
             }
+            this.draw();
         }
-        this.draw();
-    }
-};Circle.prototype.draw=function(){
-    ctx.beginPath();
-    ctx.arc(this.x , this.y ,this.r,0,2*Math.PI);
-    ctx.fillStyle=this.color;
-    ctx.fill();
-    ctx.closePath();
+    };
+    this.draw=function(){
+        ctx.beginPath();
+        ctx.arc(this.x , this.y ,this.r,0,2*Math.PI);
+        ctx.fillStyle=this.color;
+        ctx.fill();
+        ctx.closePath();
+    };
 };
 window.onresize=function () {
   _w=J.width();
